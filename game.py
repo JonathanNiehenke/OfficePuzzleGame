86edc5bc12896cef4dca05d9a533844f476d9288
Jonathan <Jonathan>
2016-06-23 09:42:33 -0700

    Rename images and include source and end tiles
    - source and end tiles includes OfficeGame:
      - attributes: level_requirements, end_cell, end_tile, fill_tile
      - methods: grab_source, update_end_cell, finish.

--- game.py
+++ game.py
@@ -1,39 +1,92 @@
+from collections import defaultdict
 import tkinter as tk
 
 import tile_game_engine
+import office_levels
+
+db = print
 
 class OfficeGame(object):
 
-    def __init__(self, Parent, playerTile, Level):
-        MapTile = tile_game_engine.MapTile
-        Tiles = {
-            '$': MapTile('$', 'Images\Player.png', None),
-            '#': MapTile('#', 'Images\Wall.png', None),
-            ' ': MapTile(' ', 'Images\Empty.png', self.move_player),
+    def __init__(self, Parent, playerTileType, Levels):
+        self.parent = Parent
+        MapTile, imagePath = tile_game_engine.MapTile, 'Images\\{}.png'.format
+        # Vim marcro: >>."zyi'f(sMapTile('z', imagePath(f,i)wiself.
+        self.tiles = {
+            '$': MapTile('$', imagePath('Player'), None),
+            '#': MapTile('#', imagePath('Wall'), None),
+            ' ': MapTile(' ', imagePath('Empty'), self.move_player),
+            'a': MapTile('a', imagePath('Key_a'), self.pickup_item),
+            'b': MapTile('b', imagePath('Key_b'), self.pickup_item),
+            'c': MapTile('c', imagePath('Key_c'), self.pickup_item),
+            'd': MapTile('d', imagePath('Key_d'), self.pickup_item),
+            'A': MapTile('A', imagePath('Lock_A'), self.open_cell),
+            'B': MapTile('B', imagePath('Lock_B'), self.open_cell),
+            'C': MapTile('C', imagePath('Lock_C'), self.open_cell),
+            'D': MapTile('D', imagePath('Lock_D'), self.open_cell),
+            'e': MapTile('e', imagePath('Source'), self.grab_source),
+            'E': MapTile('E', imagePath('Elevator'), self.finish),
             }
-        self.parent, self.level = Parent, Level
-        self.game_environment = tile_game_engine.MapFrame(Parent, Tiles, playerTile)
-        self.game_environment.build(Level)
-        self.parent.bind("<Key>", self.game_environment.handle_input)
+        self.fill_tile, self.end_tile = self.tiles[' '], self.tiles['E']
+        self.environment = tile_game_engine.NavigationalFrame(
+            Parent, self.tiles, playerTileType)
+        self.environment.pack()
+        self.generated_levels = iter(self.prep_levels(Levels))
+        self.level_requirements, self.end_cell = next(self.generated_levels)
+        self.inventory = tile_game_engine.InventoryFrame(Parent)
+        self.inventory.pack()
+        self.inventory.initialize_group("Keys", self.fill_tile, 4)
+        self.inventory.initialize_group("Hands", self.fill_tile, 2)
+        self.parent.bind("<Key>", self.environment.handle_input)
+
+    def prep_levels(self, Levels):
+        for Title, Structure, *Messages in Levels:
+            self.environment.build(Structure)
+            Requirements = len(self.environment.cell_locations.get('e', []))
+            End = self.environment.cell_locations.get('E', [None])[-1]
+            # Prevent defaulting an open end at starting location.
+            if not End and Requirements:
+                End = self.environment.cell_locations['$'][-1]
+            yield Requirements, self.environment.cells[End]
 
     def move_player(self, moveTo, cellTo):
-        self.game_environment.move_player(moveTo, cellTo)
+        self.environment.move_player(moveTo, cellTo)
 
-    def open_cell(self, moveTo, cellTo):
-        requiredKey = cellTo.type.lower()
+    def pickup_item(self, moveTo, cellTo):
+        self.inventory.include("Keys", cellTo.tile)
+        cellTo.replace_tile(self.fill_tile)
+        self.move_player(moveTo, cellTo)
         
+    def open_cell(self, moveTo, cellTo):
+        requiredKey = self.tiles[cellTo.type.lower()]
+        if self.inventory.is_carrying("Keys", requiredKey):
+            self.inventory.remove("Keys", requiredKey)
+            cellTo.replace_tile(self.fill_tile)
+            self.move_player(moveTo, cellTo)
+
+    def grab_source(self, moveTo, cellTo):
+        cellTo.replace_tile(self.fill_tile)
+        self.level_requirements -= 1
+        self.update_end_cell()
+        self.move_player(moveTo, cellTo)
+        # self.message.set('Source information obtained.')
+
+    def update_end_cell(self):
+        if self.level_requirements:
+            self.end_cell.replace_tile(self.fill_tile)
+        else:
+            self.end_cell.replace_tile(self.end_tile)
+
+    def finish(self, moveTo, cellTo):
+        self.move_player(moveTo, cellTo)
+        self.environment.reset()
+        self.level_requirements, self.end_cell = next(self.generated_levels)
+
 
 def main():
     gameWindow = tk.Tk()
-    OfficeGame(gameWindow, '$', Level)
+    OfficeGame(gameWindow, '$', office_levels.levels)
     gameWindow.focus_force()
     gameWindow.mainloop()
-
-Level = (
-    '#####',
-    '#$  #',
-    '### #',
-    '#   #',
-    '#####')
 
 main()
--- tile_game_engine.py
+++ tile_game_engine.py
@@ -1,4 +1,4 @@
-from collections import defaultdict, namedtuple, OrderedDict
+from collections import defaultdict, namedtuple
 from itertools import chain, cycle
 
 import tkinter as tk
@@ -16,38 +16,48 @@ class MapTile(object):
     def action(self, moveTo, cellTo):
         try:
             self.actionFunction(moveTo, cellTo)
-        except TypeError:  # Circumstancually None.
+        except TypeError:  # Circumstantially None.
             pass
+
+    def __str__(self):
+        return '<MapTile: {}>'.format(self.type)
 
 
 class MapCell(object):
     """
     An object consisting of a MapTile object and a tk.Label of an image.
+    Used where an tile and a tkImage are required.
     """
 
     def __init__(self, Tile, tkImg):
         self.tile = Tile
-        self.tkImg = tkImg
+        self.tk_image = tkImg
 
     @property
-    def type():
+    def type(self):
         return self.tile.type
 
     def action(self, moveTo, cellTo):
-        return self.tile.action(moveTo, cellTo)
+        self.tile.action(moveTo, cellTo)
 
     def replace_tile_image(self, tileImage):
-        self.tkImg.configure(image=tileImage)
+        self.tk_image.configure(image=tileImage)
 
     def reset_tile_image(self):
-        self.tkImg.configure(image=self.tile.image)
+        self.tk_image.configure(image=self.tile.image)
 
     def replace_tile(self, Tile):
         self.tile = Tile
-        self.tkImg.configure(Tile.image)
+        self.tk_image.configure(image=Tile.image)
+
+    def __str__(self):
+        return "<MapCell: {}>".format(self.type)
+
+    def __repr__(self):
+        return self.__str__()
 
 
-class MapFrame(tk.Frame):
+class NavigationalFrame(tk.Frame):
     """
     A continuous tk.Frame that displays and manages the graphical
     representation of the environments above view. Handles level loads,
@@ -66,17 +76,16 @@ class MapFrame(tk.Frame):
 
     def __init__(self, Parent, Tiles, playerTileType, *args, **kwargs):
         tk.Frame.__init__(self, Parent, *args, **kwargs)
-        self.pack()
         self.parent, self.tiles = Parent, Tiles
         self.player_tile = Tiles[playerTileType]
-        self.cells, self.cell_locations = {}, defaultdict(list)
         self.player_i, self.player_j = None, None
+        self.cells, self.cell_locations = {}, defaultdict(list)
 
     def build(self, Structure):
         for Index, tileType in self.iter_2d(Structure):
             self.cells[Index] = self.initiate_cell(Index, tileType)
             self.cell_locations[tileType].append(Index)
-        startIndex = self.cell_locations[self.player_tile.type][0]
+        startIndex = self.cell_locations[self.player_tile.type][-1]
         self.cells[startIndex] = self.initiate_cell(
             self.gridIndex(*startIndex), ' ')
         self.cells[startIndex].replace_tile_image(self.player_tile.image)
@@ -94,7 +103,7 @@ class MapFrame(tk.Frame):
                 yield self.gridIndex(Row, Col), tileType
 
     def handle_input(self, Event):
-        """Handle directoinal keypress or escape."""
+        """Handle directional key-press or escape."""
         keyPressed = Event.keysym
         try:
             I, J = self.directional_controls[keyPressed]
@@ -112,13 +121,53 @@ class MapFrame(tk.Frame):
         cellTo.replace_tile_image(self.player_tile.image)
         self.player_i, self.player_j = moveTo
 
-"""
+    def reset(self):
+        for child in self.winfo_children():
+            child.destroy()
+        self.cells.clear()
+        self.cell_locations.clear()
+
+
 class InventoryFrame(tk.Frame):
 
-    def __init__(self, Parent, *args, **kwargs):
-        tk.Frame.__init__(self, Parent, *args, **kwargs)
-        self.parent = Parent
+    invGroup = namedtuple("inventoryGroup", ["fill", "slots"])
 
-    def include_inventory_group(self, groupName, )
-        self.inventory[groupName]
-"""
+    def __init__(self, Parent, Arranged='Vertically', *args, **kwargs):
+        tk.Frame.__init__(self, Parent, *args, **kwargs)
+        self.parent, self.inventory = Parent, {}
+        self.group_pack = ('left' if Arranged == 'Horizontally' else 'top')
+
+    def initialize_group(self, groupName, fillTile, slotAmount,
+                         Arranged='Horizontally', *args, **kwargs):
+        Slots, groupFrame = [], tk.Frame(self.parent, *args, **kwargs)
+        groupFrame.pack(side=self.group_pack)
+        for _ in range(slotAmount):
+            tkImg = tk.Label(groupFrame, image=fillTile.image)
+            tkImg.pack(side=('top' if Arranged == 'Vertically' else 'left'))
+            Slots.append(MapCell(fillTile, tkImg))
+        self.inventory[groupName] = self.invGroup(fillTile, Slots)
+
+    def is_carrying(self, groupName, Tile):
+        for Cell in self.inventory[groupName].slots:
+            if Tile == Cell.tile:
+                return True
+        else:
+            return False
+
+    def include(self, groupName, includedTile):
+        self.replace(groupName, self.inventory[groupName].fill, includedTile)
+
+    def remove(self, groupName, replaceTile):
+        self.replace(groupName, replaceTile, self.inventory[groupName].fill)
+
+    def replace(self, groupName, removedTile, includedTile):
+        removedTileType = removedTile.type
+        for Cell in self.inventory[groupName].slots:
+            if Cell.type == removedTileType:
+                Cell.replace_tile(includedTile)
+                break
+
+    def reset(self):
+        for fillTile, Slots in self.inventory:
+            for Cell in Slots:
+                Cell.replace_tile(fillTile)
