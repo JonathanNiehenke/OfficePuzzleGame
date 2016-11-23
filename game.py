1da1297f61fe6d3a5edb33723037b3cb28338e6a
JonathanNiehenke <jrniehenke@gmail.com>
2016-11-23 13:25:57 -0800

    Include docstring comments

--- game.py
+++ game.py
@@ -8,18 +8,18 @@ db = print
 
 # Objectives
 #   Source: Position, printing.
-#   Preceeding: Computer, Power, Password, USB.
+#   Preceding: Computer, Power, Password, USB.
 #   Return items: Mop, flashlight.
 #   Reset state: Light, motion.
 # Obstructions
 #   Removable: Key locks, wet floor.
 #   Togglable: Lights, Motion detectors.
 #   Portable: Cart, Plant, Papers, Garbage.
-#   Preceeding: Power, Pin, replace bulb, new batteries, soap and water.
+#   Preceding: Power, Pin, replace bulb, new batteries, soap and water.
 # Reroute
 #   Emergency: Bathroom, dying phone battery, get boss call, wifi crash,
 #       improper temperature.
-#   Other: reminder, correcting mistake, replace air filter (Alergies).
+#   Other: reminder, correcting mistake, replace air filter (Allergies).
 # Synergistic Mechanics
 #   Once way: Flashlight-Darkness-Light, Hands-Narrow.
 # Resource:
@@ -29,9 +29,13 @@ db = print
 #   light/darkness replacement excludes player.
 #   better indicate computer before printer.
 #   better indicate changes/restrictions when holding objects.
-#   ? add restriction explinations: Darkness, WetFloor, and Plug.
+#   ? add restriction explanations: Darkness, WetFloor, and Plug.
 
 class OfficeGame(object):
+    """
+    An office themed tile puzzle game, where dark hallways, slippery
+    floors and objects prevent you from finishing your job.
+    """
 
     def __init__(self, Parent, levelFilename):
         self.parent = Parent
@@ -40,7 +44,8 @@ class OfficeGame(object):
         self.level_title = tk.StringVar()
         tk.Label(self.parent, textvar=self.level_title).pack()
         self.game_frame = tk.Frame(Parent)
-        GameTile, imagePath = tile_game_engine.GameTile, 'Images\\{}.png'.format
+        GameTile = tile_game_engine.GameTile
+        imagePath = 'Images\\{}.png'.format
         self.tiles = {
             '$': GameTile('$', imagePath('Player'), None),
             ' ': GameTile(' ', imagePath('Empty'), self.move_player),
@@ -70,10 +75,10 @@ class OfficeGame(object):
             'K': GameTile('K', imagePath('Darkness'), self.enter_cell),
             'l': GameTile('l', imagePath('LightOff'), self.turn_light_on),
             'L': GameTile('L', imagePath('LightOn'), self.turn_light_off),
-            'q': GameTile('q', imagePath('LightPlug'), self.accept_hand_item),
+            'q': GameTile('q', imagePath('LightPlug'), self.swap_plug),
             'Q': GameTile('Q', imagePath('Empty'), self.limit_plug),
-            'r': GameTile('r', imagePath('ComputerPlug'), self.accept_hand_item),
-            'R': GameTile('R', imagePath('PrinterPlug'), self.accept_hand_item),
+            'r': GameTile('r', imagePath('ComputerPlug'), self.swap_plug),
+            'R': GameTile('R', imagePath('PrinterPlug'), self.swap_plug),
             's': GameTile('s', imagePath('Socket'), self.plug_in),
             'S': GameTile('S', imagePath('PluggedSocket'), self.remove_plug),
             'p': GameTile('p', imagePath('Computer'), self.print_source),
@@ -91,9 +96,10 @@ class OfficeGame(object):
         self.keys.grid(row=2, column=0)
         self.hands.grid(row=2, column=1)
         self.game_frame.pack()
-        self.parent.bind("<Key>", self.__handle_input)
+        self.parent.bind("<Key>", self.__handle_key)
 
     def __build_inventories(self):
+        """Builds the key, hands and map inventory groups."""
         keyTiles = [self.tiles[tileType] for tileType in ("abcd")]
         Keys = tile_game_engine.InventorySlots(
             self.game_frame, keyTiles, "left", self.fill_tile, Shared=False)
@@ -105,10 +111,11 @@ class OfficeGame(object):
         return Keys, Hands, Map
 
     def __fileParser(self, fileName):
+        """Iterable of parsed levels from fileName."""
         with open(fileName, "r") as File:
             Messages, Level = [], []
             for Line in File:
-                Begins = Line[0]
+                Begins = Line[0]  # Empty lines contain a newline.
                 if Begins == '"':
                     Messages.append(Line.strip())
                 elif Begins == '\n' and Level:
@@ -120,20 +127,8 @@ class OfficeGame(object):
                 else:
                     Level.append(Line.strip())  # removes newline.
 
-    def __prep_levels(self, levelFilename):
-        for Messages, Structure in self.__fileParser(levelFilename):
-            self.__display_messages(Messages)
-            self.environment.build(Structure)
-            sourceCount = self.environment.count_tile_types('e')
-            printerCount = self.environment.count_tile_types('p')
-            uPrinterCount = self.environment.count_tile_types('R')
-            Requirements = sourceCount + printerCount + uPrinterCount
-            End = self.environment.cell_locations.get('E', [None])[-1]
-            if End is None and Requirements:
-                End = self.environment.cell_locations['$'][-1]
-            yield Requirements, self.environment.cells[End]
-
     def __display_messages(self, Messages):
+        """Display pre-level messages in the place of game_frame."""
         Title, *Information = Messages
         self.level_title.set(Title)
         self.game_frame.pack_forget()
@@ -142,7 +137,22 @@ class OfficeGame(object):
             self.parent.wait_window(Temp)
         self.game_frame.pack()
 
+    def __prep_levels(self, levelFilename):
+        """Load level, their messages and their requirements."""
+        for Messages, Structure in self.__fileParser(levelFilename):
+            self.__display_messages(Messages)
+            self.environment.build(Structure)
+            End = self.environment.cell_locations.get('E', [None])[-1]
+            sourceCount = self.environment.count_tile_types('e')
+            printerCount = self.environment.count_tile_types('p')
+            uPrinterCount = self.environment.count_tile_types('R')
+            Requirements = sourceCount + printerCount + uPrinterCount
+            if End is None and Requirements:
+                End = self.environment.cell_locations['$'][-1]
+            yield Requirements, self.environment.cells[End]
+
     def __change_requirements(self, value):
+        """Change requirements and reveal or hide the elevator."""
         self.level_requirements += value
         if self.level_requirements:
             self.end_cell.replace_tile(self.fill_tile)
@@ -150,16 +160,19 @@ class OfficeGame(object):
             self.end_cell.replace_tile(self.end_tile)
 
     def __accept_object(self, tileType, Tile):
+        """Replaces the inventory and adjusts the environment."""
         self.hands.replace(Tile.type, Tile)
         self.__change_requirements(1)
         self.environment.replace_tiles(':', self.tiles[';'])
 
     def __drop_object(self, tileType):
+        """Removes the inventory and adjusts the environment."""
         self.hands.remove(tileType)
         self.__change_requirements(-1)
         self.environment.replace_tiles(':', self.tiles[':'])
 
-    def __handle_input(self, Event):
+    def __handle_key(self, Event):
+        """Handles keyboard input to quit or move in direction."""
         keyPressed = Event.keysym
         if (keyPressed == "Escape" and
                 messagebox.askyesno("Quit?", "You want to quit?")):
@@ -171,12 +184,14 @@ class OfficeGame(object):
         self.environment.move_player(moveTo, cellTo)
 
     def pickup_key(self, moveTo, cellTo):
+        """Replaces cell to Empty and place key in inventory."""
         Tile = cellTo.tile
         self.keys.replace(Tile.type, Tile)
         cellTo.replace_tile(self.fill_tile)
         self.move_player(moveTo, cellTo)
-        
+
     def open_cell(self, moveTo, cellTo):
+        """Replaces cell and inventory if carrying the required key."""
         requiredKey = self.tiles[cellTo.type.lower()]
         if self.keys.is_carrying(requiredKey.type):
             self.keys.remove(requiredKey.type)
@@ -184,26 +199,31 @@ class OfficeGame(object):
             self.move_player(moveTo, cellTo)
 
     def enter_cell(self, moveTo, cellTo):
+        """Move player if carrying the required key."""
         if self.hands.is_carrying(cellTo.type.lower()):
             self.move_player(moveTo, cellTo)
 
     def grab_source(self, moveTo, cellTo):
+        """Replaces cell, moves player and adjusts the environment."""
         cellTo.replace_tile(self.fill_tile)
         self.__change_requirements(-1)
         self.move_player(moveTo, cellTo)
         # self.message.set('Source information obtained.')
 
     def finish(self, moveTo, cellTo):
+        """Loads the next level or congratulates then closes."""
         self.move_player(moveTo, cellTo)
         self.map.clear()
         self.environment.reset()
         try:
-            self.level_requirements, self.end_cell = next(self.generated_levels)
+            self.level_requirements, self.end_cell = next(
+                self.generated_levels)
         except StopIteration:
-            tk.messagebox.showinfo("Complete", "You finished?")
+            tk.messagebox.showinfo("Complete", "You finished!")
             self.parent.destroy()
 
     def swap_items(self, moveTo, cellTo):
+        """Swaps, drops or accepts objects and other adjustments."""
         handItem = self.hands.item(cellTo.tile.type)
         dropItem = self.map.item(moveTo, cellTo.tile.type)
         self.hands.replace(handItem, self.tiles[dropItem])
@@ -218,6 +238,7 @@ class OfficeGame(object):
             self.environment.replace_tiles(':', self.tiles[';'])
 
     def pickup_item(self, moveTo, cellTo):
+        """Replaces cell and moves player if hands have nothing."""
         if self.hands.is_carrying('@'):
             Tile = cellTo.tile
             self.__accept_object(Tile.type, Tile)
@@ -225,21 +246,25 @@ class OfficeGame(object):
             self.move_player(moveTo, cellTo)
 
     def drop_item(self, moveTo, cellTo):
+        """Remove object from hands if carrying the intended object."""
         item = cellTo.type.upper()
         if self.hands.is_carrying(item):
             self.__drop_object(item)
 
     def remove_slip(self, moveTo, cellTo):
+        """Replaces and moves player if carrying the required key."""
         requiredKey = self.tiles[cellTo.type.lower()]
         if self.hands.is_carrying(requiredKey.type):
             cellTo.replace_tile(self.fill_tile)
             self.move_player(moveTo, cellTo)
 
     def limit_plug(self, moveTo, cellTo):
+        """Moves player if not carrying a plug."""
         if not self.hands.item('@') in "qRr":
             self.move_player(moveTo, cellTo)
 
-    def accept_hand_item(self, moveTo, cellTo):
+    def swap_plug(self, moveTo, cellTo):
+        """Accepts or drops plug type."""
         if self.hands.is_carrying('@'):
             Tile = cellTo.tile
             self.__accept_object(Tile.type, Tile)
@@ -247,6 +272,7 @@ class OfficeGame(object):
             self.__drop_object(cellTo.type)
 
     def plug_in(self, moveTo, cellTo):
+        """Drops plug type and adjusts the environment."""
         Plug = self.hands.item('@')
         if Plug in "qRr":
             self.__drop_object(Plug)
@@ -257,22 +283,25 @@ class OfficeGame(object):
                 self.environment.cells[cellIndex].replace_tile(Off)
 
     def remove_plug(self, moveTo, cellTo):
+        """Accepts plug type and adjusts environment."""
         Plug = self.map.item(moveTo)
         On = {'q': 'L', 'r': 'p', 'R': 'P'}[Plug]
         Unpowered = all(
             cellTypes != On for cellTypes in self.environment.iter_types(Plug))
         if self.hands.is_carrying('@') and Unpowered:
             plugTile = self.tiles[Plug]
+            self.__accept_object(Plug, plugTile)
             self.map.remove(moveTo)
             cellTo.replace_tile(self.tiles['s'])
             self.environment.replace_tiles(Plug, plugTile)
-            self.__accept_object(Plug, plugTile)
 
     def print_source(self, moveTo, cellTo):
+        """Replaces cell and adds print to map inventory."""
         self.map.replace(cellTo.type, cellTo.tile)
         cellTo.replace_tile(self.tiles['#'])
 
     def grab_print(self, moveTo, cellTo):
+        """Replaces and changes requirements if map carries print."""
         if self.map.is_carrying('p'):
             self.map.remove('p')
             cellTo.replace_tile(self.tiles['#'])
@@ -280,19 +309,18 @@ class OfficeGame(object):
             # self.message.set('Source information obtained.')
 
     def turn_light_on(self, moveTo, cellTo):
+        """Turns light on and adjusts environment and requirements."""
         cellTo.replace_tile(self.tiles['L'])
         self.environment.replace_tiles('K', self.fill_tile,)
         self.__change_requirements(1)
 
     def turn_light_off(self, moveTo, cellTo):
+        """Turns light off and adjusts environment and requirements."""
         cellTo.replace_tile(self.tiles['l'])
         self.environment.replace_tiles('K', self.tiles['K'])
         self.__change_requirements(-1)
 
-def main():
-    gameWindow = tk.Tk()
-    OfficeGame(gameWindow, "office_levels.txt")
-    gameWindow.focus_set()
-    gameWindow.mainloop()
-
-main()
+gameWindow = tk.Tk()
+OfficeGame(gameWindow, "office_levels.txt")
+gameWindow.focus_set()
+gameWindow.mainloop()
--- tile_game_engine.py
+++ tile_game_engine.py
@@ -8,7 +8,7 @@ db = print
 
 
 class GameTile(object):
-    """An object consisting of a type, an image, and an action."""
+    """Joins referencing type and an image to an action."""
 
     def __init__(self, tileType, imagePathname, actionFunction):
         self.type = tileType
@@ -16,10 +16,11 @@ class GameTile(object):
         self.actionFunction = actionFunction
 
     def action(self, moveTo, cellTo):
+        """Initiates the associated action."""
         try:
             self.actionFunction(moveTo, cellTo)
-        except TypeError:  # Expecting NoneType.
-            pass # Explicitly supressed.
+        except TypeError:  # Action may not be associated.
+            pass # Explicitly suppressed.
 
     def __str__(self):
         return '<GameTile: {}>'.format(self.type)
@@ -27,8 +28,8 @@ class GameTile(object):
 
 class GameCell(object):
     """
-    An object joining a GameTile object and a tk.Label of an image. Used
-    where the identity or action of the tile and a tkImage are required.
+    An object joining a GameTile and the displayed image and location.
+    Used where the tile identity or action and a tkImage are required.
     """
 
     def __init__(self, Tile, tkImg):
@@ -40,15 +41,19 @@ class GameCell(object):
         return self.tile.type
 
     def action(self, moveTo, cellTo):
+        """Initiates the associated tile's action."""
         self.tile.action(moveTo, cellTo)
 
     def replace_tile_image(self, tileImage):
+        """Replaces the associated image."""
         self.tk_image.configure(image=tileImage)
 
     def reset_tile_image(self):
+        """Returns the image to the associated tile's image."""
         self.tk_image.configure(image=self.tile.image)
 
     def replace_tile(self, Tile):
+        """Replaces the tile, both associated image and action."""
         self.tile = Tile
         self.tk_image.configure(image=Tile.image)
 
@@ -71,9 +76,10 @@ class NavigationalFrame(tk.Frame):
             ['Up', 'Down', 'Left', 'Right'],
             ['w', 's', 'a', 'd'],
             ['i', 'k', 'j', 'l'],
-            ['KP_Up', 'KP_Begin', 'KP_Left', 'KP_Right']),
+            ['KP_Up', 'KP_Begin', 'KP_Left', 'KP_Right']
+        ),
         cycle([(-1 , 0), (1, 0), (0, -1), (0, 1)])
-        ))
+    ))
 
     def __init__(self, Parent, Tiles, playerTileType, *args, **kwargs):
         tk.Frame.__init__(self, Parent, *args, **kwargs)
@@ -82,39 +88,43 @@ class NavigationalFrame(tk.Frame):
         self.player_i, self.player_j = None, None
         self.cells, self.cell_locations = {}, defaultdict(list)
 
-    def build(self, Structure):
-        for Index, tileType in self.__iter_2d(Structure):
-            self.cells[Index] = self.__initiate_game_cell(Index, tileType)
-            self.cell_locations[tileType].append(Index)
-        startIndex = self.cell_locations[self.player_tile.type][-1]
-        # Properly placing player allows walkability of the startIndex.
-        self.__properly_place_player(startIndex)
-        self.player_i, self.player_j = startIndex
-
-    def count_tile_types(self, tileType):
-        return len(self.cell_locations.get(tileType, []))
-
     def __iter_2d(self, Structure):
+        """Iterable of Structure."""
         for Row, rowCells in enumerate(Structure):
             for Col, tileType in enumerate(rowCells):
                 yield self.gridIndex(Row, Col), tileType
 
     def __initiate_game_cell(self, Index, tileType):
+        """Creates new game cell at Index with tileType."""
         Tile = self.tiles[tileType]
         tkImg = tk.Label(self, image=Tile.image)
         tkImg.grid(**Index._asdict())
         return GameCell(Tile, tkImg)
 
     def __properly_place_player(self, startIndex):
+        """Replaces startIndex tile and image so that its walkable."""
         self.cells[startIndex].replace_tile(self.tiles[' '])
         self.cells[startIndex].replace_tile_image(self.player_tile.image)
 
+    def build(self, Structure):
+        """Creates and places game cells like the Structure."""
+        for Index, tileType in self.__iter_2d(Structure):
+            self.cells[Index] = self.__initiate_game_cell(Index, tileType)
+            self.cell_locations[tileType].append(Index)
+        startIndex = self.cell_locations[self.player_tile.type][-1]
+        self.__properly_place_player(startIndex)
+        self.player_i, self.player_j = startIndex
+
+    def count_tile_types(self, tileType):
+        """Counts the tiles of specified type."""
+        return len(self.cell_locations.get(tileType, []))
+
     def handle_key(self, keyPressed):
-        """Handle directional key-press."""
+        """Initiate the tile's action of the associated direction."""
         try:
             I, J = self.directional_controls[keyPressed]
-        except KeyError:
-            pass  # Explicitly silenced.
+        except KeyError:  # Not a directional control.
+            pass  # Explicitly suppressed.
         else:
             # Allows self.cells KeyError pass through if cells exist.
             if self.cells:
@@ -123,20 +133,24 @@ class NavigationalFrame(tk.Frame):
                 cellTo.action(moveTo, cellTo)
 
     def move_player(self, moveTo, cellTo):
+        """Move the player to moveTo location."""
         cellFrom = self.cells[(self.player_i, self.player_j)]
         cellFrom.reset_tile_image()
         cellTo.replace_tile_image(self.player_tile.image)
         self.player_i, self.player_j = moveTo
 
     def replace_tiles(self, tileType, Tile):
+        """Replace all initial tiles of tileType with Tile."""
         for cellIndex in self.cell_locations[tileType]:
             self.cells[cellIndex].replace_tile(Tile)
 
     def iter_types(self, tileType):
+        """Iterable of the all specified tileType."""
         for cellIndex in self.cell_locations[tileType]:
             yield self.cells[cellIndex].type
 
     def reset(self):
+        """Clears every cell and stored location."""
         for child in self.winfo_children():
             child.destroy()
         self.cells.clear()
@@ -159,11 +173,13 @@ class ItemSlot(object):
             self.__game_cell = None;
 
     def replace(self, Tile):
+        """Sets slot type and quantity."""
         self.current_type = Tile.type
         self.quantity = 1
         self.__change_image(Tile.image)
 
     def remove(self):
+        """Sets slot type to default."""
         self.current_type = self.default_tile.type
         self.quantity = 1
         self.__reset_image()
@@ -180,18 +196,18 @@ class ItemSlot(object):
     def __change_image(self, Image):
         try:
             self.__game_cell.replace_tile_image(Image)
-        except AttributeError:  # Expecting NoneType.
-            pass  # Explicitly supressed.
+        except AttributeError:  # Not intended to be displayed.
+            pass  # Explicitly suppressed.
 
     def __reset_image(self):
         try:
             self.__game_cell.reset_tile_image()
-        except AttributeError:  # Expecting NoneType.
-            pass  # Explicitly supressed.
+        except AttributeError:  # Not intended to be displayed.
+            pass  # Explicitly suppressed.
 
 
 class InventorySlots(tk.Frame):
-    """An object maintaining groups of ItemSlots."""
+    """An object maintaining a group of ItemSlots."""
 
     def __init__(self, tkParent, Tiles, Direction, fillTile, Shared):
         tk.Frame.__init__(self, tkParent)
@@ -202,6 +218,17 @@ class InventorySlots(tk.Frame):
         else:
             self.slots = {Tile.type: ItemSlot(fillTile, self, Direction)
                           for Tile in Tiles}
+
+    def __get_slot(self, Id, current):
+        try:
+            Slot = self.slots[Id]
+        except KeyError:
+            Slot = ItemSlot(self.fill_tile, self, self.direction)
+            current = self.fill_tile.type if current is None else current
+            Slot.current_type = current
+            Slot.quantity = 1
+            self.slots[Id] = Slot
+        return Slot
 
     def replace(self, Id, Tile):
         self.__get_slot(Id, Tile.type).replace(Tile)
@@ -219,21 +246,11 @@ class InventorySlots(tk.Frame):
     def item(self, Id, current=None):
         return self.__get_slot(Id, current).current_type
 
-    def __get_slot(self, Id, current):
-        try:
-            Slot = self.slots[Id]
-        except KeyError:
-            Slot = ItemSlot(self.fill_tile, self, self.direction)
-            current = self.fill_tile.type if current is None else current
-            Slot.current_type = current
-            Slot.quantity = 1
-            self.slots[Id] = Slot
-        return Slot
-
     def clear(self):
         self.slots.clear()
 
 class InscribedMessage(tk.Frame):
+    """Object displaying a message till event occurs."""
 
     def __init__(self, tkParent, Message, Wrap=0):
         tk.Frame.__init__(self, tkParent)
